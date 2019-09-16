fd5c61d26d142aa5f73d4e1827e6b0a0e3a2cfed
Evgenii Kazakov <evgenstf@gmail.com>
2019-09-16 14:31:00 +0200

    common: add travis ci

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -4,7 +4,10 @@ link_directories(/usr/local/lib)
 enable_testing()
 find_package(GTest REQUIRED)
 include_directories(${GTEST_INCLUDE_DIR})
-add_custom_target(tests COMMAND GTEST_COLOR=yes ${CMAKE_CTEST_COMMAND} --verbose)
+
+if (NOT TARGET tests)
+  add_custom_target(tests COMMAND GTEST_COLOR=yes ${CMAKE_CTEST_COMMAND} --verbose)
+endif()
 
 
 function(INIT_TEST library_name)
